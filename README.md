ad488b8429fe9d7cf5503971ef0423fe7eb1e8c7
Antti Partanen <antti.partanen@abloy.com>
2024-01-12 09:11:02 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# opt
+# Simple command line argument or opt helper
+
+See main.c for example. Compile using `gcc main.c opt.c -o opt`. Then try:
+
+```sh
+~/code/opt$ ./opt -h
+help prepend or NULL
+
+Options:
+  -h, --help                  display this help and exit
+  -f, --set-flag              set a flag
+  -c, --callback              test callback
+  -i, --int=VALUE             int
+                              (default: 0)
+  -s, --str=VALUE             str
+
+help append or NULL
+
+~/code/opt$ ./opt -f -c -i 2 -s qwerty
+test callback called
+flag: 1
+int: 2
+str: qwerty
+```
--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 static int opt_test_callback(int short_name, char *value)
 {
-    printf("test callback called");
+    printf("test callback called\n");
     return 0;
 }
 
@@ -12,15 +12,20 @@ struct opt_option opt_all[] = {
     { 'h', "help", no_argument, 0, NULL, NULL, "display this help and exit", { 0 } },
     { 'f', "set-flag", no_argument, 0, NULL, NULL, "set a flag", { 0 } },
     { 'c', "callback", no_argument, 0, NULL, opt_test_callback, "test callback", { 0 } },
-    { 'i', "int", required_argument, 0, "999", NULL, "int, defauls to 999", { OPT_FILTER_INT, 1, 4 } },
+    { 'i', "int", required_argument, 0, "0", NULL, "int", { OPT_FILTER_INT, 1, 4 } },
+    { 's', "str", required_argument, 0, NULL, NULL, "str", { 0 } },
 };
 
-int main(char *argv[], int argc)
+int main(int argc, char *argv[])
 {
     if (opt_init(opt_all, NULL, "help prepend or NULL", "help append or NULL") ||
         opt_parse(argc, argv)) {
         return 1;
     }
+
+    printf("flag: %d\n", opt_used('f'));
+    printf("int: %d\n", opt_get_int('i'));
+    printf("str: %s\n", opt_get('s'));
 
     return 0;
 }
